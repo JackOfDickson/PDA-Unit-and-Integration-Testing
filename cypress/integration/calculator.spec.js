cf630907df31b1b64cedc4a0c6deb94abe63860a
Jack <jackowendickson@gmail.com>
2022-04-11 14:17:01 +0100

    added some cypress tests

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -7,4 +7,20 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should update the display when number buttons are pressed', () => {
+    cy.get('#number1').click();
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '12');
+  })
+
+  it('should be able to chain multiple operators together', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number3').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '9')
+  })
 })
