47b94f980c82eca084b294891689827aa39f1f9d
Jack <jackowendickson@gmail.com>
2022-04-11 18:17:50 +0100

    finished cypress tests

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -23,4 +23,44 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '9')
   })
+
+  it('should be able to show a negitive number', () => {
+    cy.get('#number9').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number2').click();
+    cy.get('#number2').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number1').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-21999992');
+
+
+  })
+
+  it('should be able to show a number with an decimals', () => {
+    cy.get('#number9').click();
+    cy.get('#operator_add').click();
+    cy.get('#number1').click();
+    cy.get('#decimal').click();
+    cy.get('#number1').click();
+    cy.get('#number2').click();
+    cy.get('#number3').click();
+    cy.get('#number4').click();
+    cy.get('#number5').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '10.12345');
+
+  })
+
+  it('should return infinity when devided by zero', () => {
+    cy.get('#number1').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', 'Infinity');
+  })
 })
